bef58253d64188920667a293f62d5172bce216ab
Tara <td1159@txstate.edu>
2014-04-01 12:32:56 -0500

    Player character class

---
+++ SEProjectGame/src/characters/PlayerCharacter.java
@@ -0,0 +1,26 @@
+package characters;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class PlayerCharacter extends Entity {
+
+
+	/** The player character, has state and state time, */
+	public static class Player {
+		static float WIDTH;
+		static float HEIGHT;
+		static float MAX_VELOCITY = 10f;
+		static float JUMP_VELOCITY = 40f;
+		static float DAMPING = 0.87f;
+
+		public enum State {
+			Standing, Walking, Jumping
+		}
+
+		final Vector2 position = new Vector2();
+		final Vector2 velocity = new Vector2();
+		State state = State.Walking;
+		float stateTime = 0;
+		boolean facesRight = true;
+		boolean grounded = false;
+	}
+}
