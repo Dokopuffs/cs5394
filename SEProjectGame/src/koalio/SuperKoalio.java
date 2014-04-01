5095622b7cc3bc96ab59e98e3bbdc3e95e59b36d
Derek Knechtel <derek.knechtel@gmail.com>
2014-04-01 00:56:44 -0500

    Changes made to get a demo going.

--- SEProjectGame/src/koalio/SuperKoalio.java
+++ SEProjectGame/src/koalio/SuperKoalio.java
@@ -49,14 +49,14 @@ import com.badlogic.gdx.ApplicationListener;
  */
 public class SuperKoalio implements ApplicationListener {
 	/** The player character, has state and state time, */
-	static class Koala {
+	public static class Koala {
 		static float WIDTH;
 		static float HEIGHT;
 		static float MAX_VELOCITY = 10f;
 		static float JUMP_VELOCITY = 40f;
 		static float DAMPING = 0.87f;
 
-		enum State {
+		public enum State {
 			Standing, Walking, Jumping
 		}
 
